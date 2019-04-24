c00db3fd5f9195ad9920e51504cd69f1b2767ed7
zhou325 <u1208920@utah.edu>
2019-04-24 15:08:12 -0600

    kde

--- fp.py
+++ fp.py
@@ -12,6 +12,9 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy import stats
+from astropy.io import fits
+import cv2
+
 #from mayavi import mlab
 
 
@@ -50,7 +53,11 @@ for i in range(1,len(curves_new)):
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(curves_list[:,1], curves_list[:,2], curves_list[:,3], c='b', marker='o',s=5)
-#plt.savefig(path+"screen_shots/shape")
+plt.savefig(path+"curves")
+
+plt.scatter(curves_list[:,1], curves_list[:,2],c='b', marker='o',s=5)
+plt.savefig(path+"curves2d")
+
 
 curves_list1 = pd.DataFrame(curves_list)
 curves_list1.columns = ["id","x","y","z"]
@@ -58,13 +65,30 @@ curves_list1.columns = ["id","x","y","z"]
 ## 2d KDE 
 data  = curves_list[:,1:3]
 data = data.T
+
+X = np.arange(-70, 70, 1)
+Y = np.arange(-30, 30, 1)
+
+mesh = np.zeros(2)
+for i in range(len(X)):
+    for j in range(len(Y)):
+        mesh = np.vstack((mesh,[X[i],Y[j]]))
+mesh = mesh[1:,:]
+
 kde = stats.gaussian_kde(data)
-density = kde(data)
-x,y=data
+density = kde(mesh.T)
+x,y=mesh.T
 plt.scatter(x,y,c=density)
+plt.savefig(path+"kdeColor")
 
 fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
 ax.scatter(x, y,density, c=density)
+
+mesh = pd.DataFrame(mesh)
+mesh.columns = ["x","y"]
+mesh["2dKDE"] = density
+mesh.to_csv(path+"kdeMesh.csv",index=False)
+
 
 curves_list1["2dKDE"] = density
 
@@ -86,7 +110,68 @@ curves_list1["3dKDE"] = density1
 
 curves_list1.to_csv(path+"kde.csv",index=False)
 
+#img = cv2.imread(path+"kdeColor.jpg",0)
+#img1 = img[:,:,0]
+#high_thresh, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+#lowThresh = 0.5*high_thresh
+#edges = cv2.Canny(img1,20,20)
+#plt.imshow(edges,cmap = 'gray')
+#
+##plt.subplot(121),plt.imshow(img,cmap = 'gray')
+##plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+##plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+##plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+##
+##plt.show()
+#plt.imshow(img)
+#
+#
+#plt.imshow(edges,cmap = 'gray')
 
+def constructSp(df): # construct simplices
+    df = np.array(df)
+    sp = []
+    fmax = np.max(df[:,2])
+    for i in range(0,140):
+        for j in range(0,60):
+            # print(i,j)
+            idx = 60*j+i
+            pt1 = df[idx,:]
+            pt2 = df[idx+1,:]
+            pt3 = df[idx+60,:]
+            pt4 = df[idx+61,:]
+            line1 = str(int(round(pt1[0]*100)))+" "+str(int(round(pt1[1]*100)))+" "+str(int(round(pt2[0]*100)))+" "+str(int(round(pt2[1]*100)))+" "+str(int(round(pt3[0]*100)))+" "+str(int(round(pt3[1]*100)))+" "+str(int(round((fmax-pt1[2])*1000000000)+1))+"\n"
+            line2 = str(int(round(pt2[0]*100)))+" "+str(int(round(pt2[1]*100)))+" "+str(int(round(pt3[0]*100)))+" "+str(int(round(pt3[1]*100)))+" "+str(int(round(pt4[0]*100)))+" "+str(int(round(pt4[1]*100)))+" "+str(int(round((fmax-pt4[2])*1000000000)+1))+"\n" 
+            sp.append(line1)
+            sp.append(line2)
+    return sp
+
+mesh_persis = constructSp(mesh)
+#temp = []
+#for i in mesh_persis:
+#    i=i.split(" ")
+#    if i[6] not in temp:
+#        temp.append(i[6])
+#    else:
+#        print(i[6])
+with open(path+"mesh.txt","w") as f:
+    for line in mesh_persis:
+        f.write(line)
+    f.close()
+    
+sp = pd.read_csv(path+"mesh.txt",sep=" ",header=None)
+sp = sp.sort_values(by=[6]) # sort by birth time
+with open(path+"mesh_new.txt","w") as f:
+    f.write("2\n")
+    f.write("2\n")
+    for i in range(0,len(sp)):
+        line = list(sp.iloc[i,:])
+        s = ""
+        for e in line:
+            s+= str(e)+" "
+        s += "\n"
+        f.write(s)
+f.close()
 
 ### the following is just reference code
 
--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,56 +1,93 @@
-d3.csv("../kde.csv").then(kde=>{
+d3.csv("../kdeMesh.csv").then(kde=>{
     let svgWidth = 600;
     let svgHeight = 600;
 
     console.log(kde)
-    // construct mesh
-    let xList = [];
-    let yList = [];
-    kde.forEach(e => {
-        xList.push(parseFloat(e.x));
-        yList.push(parseFloat(e.y));
-    });
-    let xRange = {"min":Math.min.apply(null,xList),"max":Math.max.apply(null,xList)};
-    let yRange = {"min":Math.min.apply(null,yList),"max":Math.max.apply(null,yList)};
-    // console.log(xList)
-    console.log(xRange)
-    console.log(yRange)
+    let layout = {
+        // title: 'Mt Bruno Elevation',
+        autosize: false,
+        width: 600,
+        height: 600,
+        margin: {
+            l: 65,
+            r: 50,
+            b: 65,
+            t: 90
+        }
+    };
+
+    let data = [
+        {
+          z: [],
+          type: 'surface'
+        }
+      ];
+    
+    for(let i=0;i<140;i++){
+        let zz = [];
+        for(let j=0;j<60;j++){
+            // console.log(i,j)
+            zz.push(kde[i*60+j]["2dKDE"])
+        }
+        data[0].z.push(zz)
+    }
+    console.log(data)
+
+    Plotly.newPlot('container', data, layout);
+    // d3.select("#js-plotly-tester")
+        // .style("z-index",7)
+        // .style("width",600)
+        // .style("height",600)
+
+    // console.log(kde)
+    // // construct mesh
+    // let xList = [];
+    // let yList = [];
+    // kde.forEach(e => {
+    //     xList.push(parseFloat(e.x));
+    //     yList.push(parseFloat(e.y));
+    // });
+    // let xRange = {"min":Math.min.apply(null,xList),"max":Math.max.apply(null,xList)};
+    // let yRange = {"min":Math.min.apply(null,yList),"max":Math.max.apply(null,yList)};
+    // // console.log(xList)
+    // console.log(xRange)
+    // console.log(yRange)
     let xRange_new = {"min":-70,"max":70};
     let yRange_new = {"min":-30,"max":30};
-    let N = 100;
-    let xStep = (xRange_new.max-xRange_new.min)/N;
-    let yStep = (yRange_new.max-yRange_new.min)/N;
-    let xp = d3.range(N).map(i=>{
-        return xRange_new.min + xStep*i;
-    })
-    let yp = d3.range(N).map(i=>{
-        return yRange_new.min + yStep*i;
-    })
-    console.log(xp,yp)
-    let mesh_2d = []
-    for(let i=0;i<xp.length;i++){
-        for(let j=0;j<yp.length;j++){
-            mesh_2d.push({"x":xp[i],"y":yp[j],"kde":0})
-        }
-    }
+    // let N = 100;
+    let xStep = (xRange_new.max-xRange_new.min)/140;
+    let yStep = (yRange_new.max-yRange_new.min)/60;
+    // let xp = d3.range(N).map(i=>{
+    //     return xRange_new.min + xStep*i;
+    // })
+    // let yp = d3.range(N).map(i=>{
+    //     return yRange_new.min + yStep*i;
+    // })
+    // console.log(xp,yp)
+    // let mesh_2d = []
+    // for(let i=0;i<xp.length;i++){
+    //     for(let j=0;j<yp.length;j++){
+    //         mesh_2d.push({"x":xp[i],"y":yp[j],"kde":0})
+    //     }
+    // }
 
     //assign KDE value to each mesh
     let maxKDE = 0;
     for(let i=0;i<kde.length;i++){
-        let x = parseFloat(kde[i].x)
-        let y = parseFloat(kde[i].y)
+    //     let x = parseFloat(kde[i].x)
+    //     let y = parseFloat(kde[i].y)
         let d = parseFloat(kde[i]["2dKDE"])
-        // console.log(d)
-        let xId = Math.floor((x-xRange_new.min)/xStep);
-        let yId = Math.floor((y-yRange_new.min)/yStep);
-        let meshId = xId*100+yId; 
+    //     // console.log(d)
+    //     let xId = Math.floor((x-xRange_new.min)/xStep);
+    //     let yId = Math.floor((y-yRange_new.min)/yStep);
+    //     let meshId = xId*100+yId; 
         if(d>maxKDE){
             maxKDE = d;
         }
-        // *** TODO: some point may fall in the same mesh
-        mesh_2d[meshId].kde = d;
+    //     // *** TODO: some point may fall in the same mesh
+    //     mesh_2d[meshId].kde = d;
     }
-    console.log(mesh_2d)
+    // console.log(mesh_2d)
 
     let xMap = d3.scaleLinear()
                 .domain([xRange_new.min, xRange_new.max])
@@ -61,11 +98,12 @@ d3.csv("../kde.csv").then(kde=>{
 
     let colorMap = d3.scaleLinear()
                     .domain([0, maxKDE])
-                    .range(['white', 'red']);
+                    .range(["blue", 'red']);
 
     d3.select("#kde").append("g")
         .attr("id","meshgroup");
-    let kdeRect = d3.select("#meshgroup").selectAll("rect").data(mesh_2d);
+    
+    let kdeRect = d3.select("#meshgroup").selectAll("rect").data(kde);
     kdeRect.exit().remove();
     let newKdeRect = kdeRect.enter().append("rect");
     kdeRect = newKdeRect.merge(kdeRect);
@@ -74,11 +112,53 @@ d3.csv("../kde.csv").then(kde=>{
         .attr("y",d=>yMap(d.y))
         .attr("width",xMap(xStep))
         .attr("height",yMap(yStep))
-        .attr("fill",d=>colorMap(d.kde))
+        .attr("fill",d=>colorMap(d["2dKDE"]))
+        // .style("visibility","hidden")
         // .attr("fill","red")
-        // .style("opacity",0.1)
+        // .style("visibility","hidden")
+    // let ul=d3.select('#container')
+    //        .append('ul');
+    
+    // let surfaceGroup = d3.select("#kde").append("g")
+    //                     .attr("id","surfacegroup");
+    // surfaceGroup.data(mesh_2d)
+    //     .surface3D(svgWidth,svgHeight)
+    //     .surfaceHeight(function(d){ 
+    //         return d;
+    //       }).surfaceColor(function(d){
+    //         var c=d3.hsl((d+100), 0.6, 0.5).rgb();
+    //         return "rgb("+parseInt(c.r)+","+parseInt(c.g)+","+parseInt(c.b)+")";
+    //       });
+    // d3.select("#kde").on("mousedown",function(){
+    //         drag=[d3.mouse(this),yaw,pitch];
+    //     }).on("mouseup",function(){
+    //         drag=false;
+    //       }).on("mousemove",function(){
+    //         if(drag){            
+    //           var mouse=d3.mouse(this);
+    //           yaw=drag[1]-(mouse[0]-drag[0][0])/50;
+    //           pitch=drag[2]+(mouse[1]-drag[0][1])/50;
+    //           pitch=Math.max(-Math.PI/2,Math.min(Math.PI/2,pitch));
+    //           md.turntable(yaw,pitch);
+    //         }
+    //       });
+    d3.select("#kde2d")
+        .on("click",()=>{
+            console.log("clicking")
+            d3.select("#kde")
+                .style("visibility","visible");   
+            d3.select(".plot-container")
+                .style("visibility","hidden");
+        })
+    d3.select("#kde3d")
+        .on("click",()=>{
+            d3.select("#kde")
+                .style("visibility","hidden"); 
+            d3.select(".plot-container")
+                .style("visibility","visible");           
+        })
 
-    // *** TODO : need smooth for kde value, construct Morse complex
+    
 
     // *** TODO: find local maximum & saddle of Morse complex
 
